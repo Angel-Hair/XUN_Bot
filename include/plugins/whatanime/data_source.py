206b8e2f72ff0facddee125ecdd58946eeb3e04e
Lu Shihan <Angel_Hair@protonmail.com>
2020-01-21 15:45:32 +0800

    修复 识番 功能的一个bug
    修复了 识番 功能无法查询动态图片和部分集数报错的bug

--- include/plugins/whatanime/data_source.py
+++ include/plugins/whatanime/data_source.py
@@ -2,8 +2,11 @@ import json
 import requests
 
 async def get_anime(anime: str) -> str:
-    response = requests.get('https://trace.moe/api/search?url={}'.format(anime))
+    url = 'https://trace.moe/api/search?url={}'.format(anime)
+    print("[info]Now starting get the {}".format(url))
+    response = requests.get(url)
     anime_json = json.loads(response.text)
+    if anime_json == 'Error reading imagenull': return "图像源错误，注意必须是静态图片哦"
 
     repass = ""
     for anime in anime_json["docs"][:3]:
@@ -13,7 +16,7 @@ async def get_anime(anime: str) -> str:
         m, s = divmod(at, 60)
         similarity = anime["similarity"]
 
-        putline = "[ {} ][{:0>2d}][{}:{}] 相似度:{:.2%}".format(anime_name, episode, m, s, similarity)
+        putline = "[ {} ][{}][{}:{}] 相似度:{:.2%}".format(anime_name, episode if episode else '?', m, s, similarity)
         if repass:
             repass = "\n".join([repass, putline])
         else:
