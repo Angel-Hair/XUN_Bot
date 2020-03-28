4c656aa933e2fb845eb9ccafe0fc82e675cdab74
Lu Shihan <Angel_Hair@protonmail.com>
2020-03-28 23:07:53 +0800

    增加 日语词典 功能
    增加 日语词典 功能

--- config.py
+++ config.py
@@ -17,8 +17,11 @@ CEICONLYCN = True # 是否只报道国内地震 | 类型为bool
 RECOMMENDER_MUSIC = False # 音乐推荐功能的回复是否显示推荐者 | 类型为bool
 PLAYLIST_MUSIC = True # 音乐推荐功能的回复是否显示来源歌单 | 类型为bool
 MAXINFO_REIMU = 3 # 上车功能查找目的地的最大数 | 类型为int>0
+MAXLINE_JD = 7 # 日语词典功能查找条目的内容所允许的最大行书 | 类型为int>0
+MAXWOED_JD = 300 # 日语词典功能查找条目的内容所允许的最大字数 | 类型为int>0
 TIMELIMIT_IMAGE = 7 # 识图功能的时间限制 | 类型为float
 TIMELIMIT_REIMU = 12 # 上车功能的时间限制 | 类型为float
+TIMELIMIT_JD = 7 # 日语词典功能的时间限制 | 类型为float
 MORE_COMPLEX = False # 是否提供更加复杂的计算库 | 类型为bool
 CALCULATE_LIST = {
     'numpy':'np',
---
+++ include/plugins/japanese_dictionary/data_source.py
@@ -0,0 +1,57 @@
+from lxml import etree
+import requests
+import sys
+import re
+
+sys.path.append('../../../')
+from config import TIMELIMIT_JD, MAXINFO_JD, MAXWOED_JD
+from kth_timeoutdecorator import *
+
+@timeout(TIMELIMIT_JD)
+async def get_definition_of_word(word: str) -> str:
+    is_hidden = False
+    url = 'https://www.weblio.jp/content/' + word
+    print("[info] Now get definition from: {}".format(url))
+    html_data = requests.get(url)
+    html = etree.HTML(html_data.text)
+
+    notfound = html.xpath('//div[@id="nrCntTH"]/p/text()')
+    if notfound:
+        return notfound[0]
+
+    source = html.xpath('//div[@class="pbarTL"]/a/text()')
+    definition = []
+    d_html = html.xpath('//div[@class="kiji"]')
+    for d in d_html:
+        definition.append(d.xpath('string(.)').replace("  ","\n").replace("\n\n","\n").strip())
+    
+    repass = ""
+    putline = []
+    for s, d in zip(source, definition):
+        line_is_hidden = False
+        d = d.replace("\n ", "\n")
+        pattern_1 = re.compile(r'(document.write)(.*)(\);)')
+        pattern_2 = re.compile(r'(\n出典)(.*)(\))')
+        d = pattern_1.sub(r'', d)
+        d = pattern_2.sub(r'', d)
+        d = re.sub(r'\n.\n', "\n", d)
+        line_num = d.count("\n")
+        if line_num > MAXINFO_JD:
+            is_hidden = True
+            line_is_hidden = True
+            index = 0
+            for i in range(MAXINFO_JD):
+                index = d.find("\n", index+1)
+            d = d[:index].strip()
+        if len(d) > MAXWOED_JD:
+            is_hidden = True
+            line_is_hidden = True
+            d = d[:MAXWOED_JD].strip()
+        if line_is_hidden:
+            d += "……\n[Note]……有内容被省略……"
+        putline.append("【{}】\n{}".format(s,d))
+    repass = "\n\n".join(putline)
+    if is_hidden:
+        repass += "\n\n[Note]有条目过长被省略，更多释义请参考: {}".format(url)
+
+    return repass
